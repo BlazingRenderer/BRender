c65f63184929aa88c28353fa5ee2e9e45e80db98
Zane van Iperen <zane@zanevaniperen.com>
2022-09-15 02:21:04 +1000

    nix: refactor flake and add darwin targets

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
   description = "BRender";
 
-  outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in rec {
+  outputs = { self, nixpkgs, ... }: let
+    mkPackages = { pkgs }: rec {
       brender-samples = pkgs.callPackage ./default.nix {
         version = self.lastModifiedDate;
       };
 
       default = brender-samples;
     };
+  in {
+    packages.x86_64-linux   = mkPackages { pkgs = import nixpkgs { system = "x86_64-linux";   }; };
+    packages.x86_64-darwin  = mkPackages { pkgs = import nixpkgs { system = "x86_64-darwin";  }; };
+    packages.aarch64-darwin = mkPackages { pkgs = import nixpkgs { system = "aarch64-darwin"; }; };
   };
 }
