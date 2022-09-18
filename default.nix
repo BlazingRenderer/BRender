fc666207cb736f8d2c6ccea2932fde829514d6a6
Zane van Iperen <zane@zanevaniperen.com>
2022-09-18 12:18:15 +1000

    meta: move mk{ranges,shares} and robots to tools/ and examples/
    Allow disabling them with BRENDER_BUILD_{TOOLS,EXAMPLES}=OFF.

--- default.nix
+++ default.nix
@@ -15,8 +15,8 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/{bin,share/brender-samples/robot}
-    cp mkranges/mkranges mkshades/mkshades robot/robot $out/bin
-    cp ${src}/robot/dat/* $out/share/brender-samples/robot
+    cp tools/mkranges/mkranges tools/mkshades/mkshades examples/robot/robot $out/bin
+    cp ${src}/examples/robot/dat/* $out/share/brender-samples/robot
 
     wrapProgram $out/bin/robot \
       --chdir $out/share/brender-samples/robot
