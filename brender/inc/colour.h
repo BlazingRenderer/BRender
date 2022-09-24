70d8448f3b4408a3b752913da4da73942ae5f408
Zane van Iperen <zane@zanevaniperen.com>
2022-09-25 00:39:53 +1000

    [1.3.2] brender/inc/colour: add ARGB and RGB565 helper macros

--- brender/inc/colour.h
+++ brender/inc/colour.h
@@ -30,10 +30,25 @@ typedef struct br_colour_range
 		(((br_colour)(g))<<8) |\
 		((br_colour)(b)))
 
+#define BR_COLOUR_ARGB(a,r,g,b) \
+		((((br_colour)(a))<<24) |\
+		(((br_colour)(r))<<16) |\
+		(((br_colour)(g))<<8) |\
+		((br_colour)(b)))
+
+#define BR_COLOUR_565(r,g,b) \
+		((((br_colour)(r))<<11) |\
+		(((br_colour)(g))<<5) |\
+		((br_colour)(b)))
+
 #define BR_ALPHA(c) (((c) >> 24) & 0xFF)
 #define BR_RED(c) (((c) >> 16) & 0xFF)
 #define BR_GRN(c) (((c) >> 8) & 0xFF)
 #define BR_BLU(c) ((c) & 0xFF)
+
+#define BR_RED_565(c) (((c) >> 11 & 31))
+#define BR_GRN_565(c) (((c) >> 5) & 63)
+#define BR_BLU_565(c) ((c) & 31)
 
 #endif
 
