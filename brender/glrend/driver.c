eddfaba7378d77bddefea31022c40d62798e402a
Zane van Iperen <zane@zanevaniperen.com>
2022-09-26 19:38:51 +1000

    brender/glrend: always expose a BrDrvGLBegin() symbol
    For upcoming header changes. If -DDEFINE_BR_ENTRY_POINT, then expose a
    BrDrv1Begin() symbol.

--- brender/glrend/driver.c
+++ brender/glrend/driver.c
@@ -1,9 +1,9 @@
 #include "drv.h"
 
 /*
- * Main entry point for device - this may get redefined by the build system.
+ * Main entry point for device.
  */
-br_device *BR_EXPORT BrDrv1Begin(const char *arguments)
+br_device *BR_EXPORT BrDrvGLBegin(const char *arguments)
 {
     br_device *dev = DeviceGLAllocate("opengl", arguments);
     if(!dev)
@@ -11,3 +11,10 @@ br_device *BR_EXPORT BrDrv1Begin(const char *arguments)
 
     return dev;
 }
+
+#ifdef DEFINE_BR_ENTRY_POINT
+br_device *BR_EXPORT BrDrv1Begin(const char *arguments)
+{
+    return BrDrvGLBegin(arguments);
+}
+#endif
