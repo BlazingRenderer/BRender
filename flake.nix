10f194e1f7ed2983eb61302a54677241148f5ae9
Zane van Iperen <zane@zanevaniperen.com>
2022-11-22 20:04:58 +1000

    nix: add devShells with hardening disabled

--- flake.nix
+++ flake.nix
@@ -13,5 +13,17 @@
     packages.x86_64-linux   = mkPackages { pkgs = import nixpkgs { system = "x86_64-linux";   }; };
     packages.x86_64-darwin  = mkPackages { pkgs = import nixpkgs { system = "x86_64-darwin";  }; };
     packages.aarch64-darwin = mkPackages { pkgs = import nixpkgs { system = "aarch64-darwin"; }; };
+
+    devShells.x86_64-linux.default = self.packages.x86_64-linux.default.overrideAttrs(old: {
+      hardeningDisable = [ "all" ];
+    });
+
+    devShells.x86_64-darwin.default = self.packages.x86_64-darwin.default.overrideAttrs(old: {
+      hardeningDisable = [ "all" ];
+    });
+
+    devShells.aarch64-darwin.default = self.packages.aarch64-darwin.default.overrideAttrs(old: {
+      hardeningDisable = [ "all" ];
+    });
   };
 }
