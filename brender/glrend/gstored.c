BlazingRenderer/BRender

commit 0f4abd325afeb27771b84a7747da379c92fcc723

brender/glrend: avoid some OpenGL errors

brender/glrend/gstored.c
@@ -40,17 +40,21 @@ static void create_vao(br_geometry_stored *self)
     glEnableVertexAttribArray(hVideo->brenderProgram.attributes.aPosition);
     glVertexAttribPointer(hVideo->brenderProgram.attributes.aPosition, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-    { /* gl_vertex_f */
-        glBindBuffer(GL_ARRAY_BUFFER, self->gl_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, self->gl_vbo);
 
+    if(hVideo->brenderProgram.attributes.aUV > 0) {
         glEnableVertexAttribArray(hVideo->brenderProgram.attributes.aUV);
         glVertexAttribPointer(hVideo->brenderProgram.attributes.aUV, 2, GL_FLOAT, GL_FALSE, sizeof(gl_vertex_f),
                               (void *)offsetof(gl_vertex_f, map));
+    }
 
+    if(hVideo->brenderProgram.attributes.aNormal > 0) {
         glEnableVertexAttribArray(hVideo->brenderProgram.attributes.aNormal);
         glVertexAttribPointer(hVideo->brenderProgram.attributes.aNormal, 3, GL_FLOAT, GL_FALSE, sizeof(gl_vertex_f),
                               (void *)offsetof(gl_vertex_f, n));
+    }
 
+    if(hVideo->brenderProgram.attributes.aColour > 0) {
         glVertexAttribPointer(hVideo->brenderProgram.attributes.aColour, 4, GL_UNSIGNED_BYTE, GL_TRUE,
                               sizeof(gl_vertex_f), (void *)offsetof(gl_vertex_f, c));
     }

brender/glrend/devpixmp.c
@@ -187,6 +187,12 @@ static void create_quad(br_device_pixelmap *self)
     glBindVertexArray(0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    /*
+     * Some of the above can fail if the user changes the shader.
+     * Drain all the errors to prevent them falling through to future checks.
+     */
+    while(glGetError() != GL_NO_ERROR);
 }
 
 

commit e3b0aa74277a2a6f272a7e32ede26cd8a6657898

brender/glrend: don't ignore shader attributes at index 0
Fixes: 0f4abd325afe ("brender/glrend: avoid some OpenGL errors")

brender/glrend/gstored.c
@@ -42,19 +42,19 @@ static void create_vao(br_geometry_stored *self)
 
     glBindBuffer(GL_ARRAY_BUFFER, self->gl_vbo);
 
-    if(hVideo->brenderProgram.attributes.aUV > 0) {
+    if(hVideo->brenderProgram.attributes.aUV >= 0) {
         glEnableVertexAttribArray(hVideo->brenderProgram.attributes.aUV);
         glVertexAttribPointer(hVideo->brenderProgram.attributes.aUV, 2, GL_FLOAT, GL_FALSE, sizeof(gl_vertex_f),
                               (void *)offsetof(gl_vertex_f, map));
     }
 
-    if(hVideo->brenderProgram.attributes.aNormal > 0) {
+    if(hVideo->brenderProgram.attributes.aNormal >= 0) {
         glEnableVertexAttribArray(hVideo->brenderProgram.attributes.aNormal);
         glVertexAttribPointer(hVideo->brenderProgram.attributes.aNormal, 3, GL_FLOAT, GL_FALSE, sizeof(gl_vertex_f),
                               (void *)offsetof(gl_vertex_f, n));
     }
 
-    if(hVideo->brenderProgram.attributes.aColour > 0) {
+    if(hVideo->brenderProgram.attributes.aColour >= 0) {
         glVertexAttribPointer(hVideo->brenderProgram.attributes.aColour, 4, GL_UNSIGNED_BYTE, GL_TRUE,
                               sizeof(gl_vertex_f), (void *)offsetof(gl_vertex_f, c));
     }