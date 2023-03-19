484f12caa9960f2aed1fe13c475062ead2939901
Zane van Iperen <zane@zanevaniperen.com>
2023-03-19 23:42:35 +1000

    nix: refactor derivation to support exes

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
 { stdenv, version, cmake, perl, SDL2, makeBinaryWrapper }:
+let
+  binExtension = "${stdenv.hostPlatform.extensions.executable}";
+in
 stdenv.mkDerivation rec {
   inherit version;
 
@@ -18,39 +21,34 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/{bin,share/brender-samples/{robot,dat,tutorials}}
-    cp tools/3ds2br/3ds2br $out/bin
-    cp tools/dquery/dquery $out/bin
-    cp tools/mkblend/mkblend $out/bin
-    cp tools/mkfog/mkfog $out/bin
-    cp tools/mkranges/mkranges $out/bin
-    cp tools/mkshades/mkshades $out/bin
-    cp tools/paljoin/paljoin $out/bin
-    cp tools/texconv/texconv $out/bin
-    cp tools/matconv/matconv $out/bin
-    cp examples/mkres/mkres $out/bin
-    cp examples/robot/robot $out/bin
-    cp examples/cube/cube $out/bin
-    cp examples/devpmtest/devpmtest $out/bin
+
+    for i in 3ds2br dquery mkblend mkfog mkranges mkshades paljoin texconv matconv; do
+      cp tools/$i/$i${binExtension} $out/bin
+    done
+
+    for i in mkres robot cube devpmtest; do
+      cp examples/$i/$i${binExtension} $out/bin
+    done
 
     cp ${src}/examples/robot/dat/* $out/share/brender-samples/robot
     cp ${src}/examples/dat/* $out/share/brender-samples/dat
 
-    wrapProgram $out/bin/robot \
+    wrapProgram $out/bin/robot${binExtension} \
       --chdir $out/share/brender-samples/robot
 
-    wrapProgram $out/bin/cube \
+    wrapProgram $out/bin/cube${binExtension} \
       --set BRENDER_PATH $out/share/brender-samples/dat
 
-    wrapProgram $out/bin/devpmtest \
+    wrapProgram $out/bin/devpmtest${binExtension} \
       --set BRENDER_PATH $out/share/brender-samples/dat
 
 
     cp ${src}/examples/tutorials/dat/* $out/share/brender-samples/tutorials
 
     for i in {1,2,3,4,5,5b,6,6b,7,7b,8,8b,9,10}; do
-      cp examples/tutorials/brtutor$i $out/bin
+      cp examples/tutorials/brtutor$i${binExtension} $out/bin
 
-      wrapProgram $out/bin/brtutor$i \
+      wrapProgram $out/bin/brtutor$i${binExtension} \
         --chdir $out/share/brender-samples/tutorials
     done
   '';
