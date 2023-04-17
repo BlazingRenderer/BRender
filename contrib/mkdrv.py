82f9dddee9961e952126a581c33cf4582969811e
Zane van Iperen <zane@zanevaniperen.com>
2023-04-18 03:03:13 +1000

    contrib: mkdrv.py: build a temporary include directory
    Just copy all headers into a folder, it seems to work better.

--- contrib/mkdrv.py
+++ contrib/mkdrv.py
@@ -7,44 +7,25 @@ import tempfile
 import copy
 import re
 import argparse
+import shutil
 from typing import List
 
-def get_short_path(long_name):
-	#print '%-16s%s' % ('long_name: ', long_name)
-	for_cmd = 'for %I in ("' + long_name + '") do echo %~sI'
-	#print '%-16s%s' % ('for_cmd: ', for_cmd)
 
-	p = os.popen(for_cmd)
-	short_name = p.readlines()[-1] # last line from for command
+def build_local_include_dir(tmpdir: str, paths: List[str]) -> str:
+	incpath = os.path.join(tmpdir, 'include')
 
-	if p.close():
-		print('Error calling shell command "for"')
-		raise Exception
-		
-	return short_name.replace('\n', '').replace('\r', '')
+	os.makedirs(incpath, exist_ok=True)
+	for p in paths:
+		for path, dirs, files in os.walk(p):
+			for f in files:
+				_, ext = os.path.splitext(f)
+				if ext.lower() not in ['.h', '.cgh']:
+					continue
 
+				file = os.path.join(path, f)
+				shutil.copyfile(file, os.path.join(incpath, f))
 
-def remove_paths(includes: List[str]) -> List[str]:
-	newpaths = []
-
-	for path in includes:
-		comps = path.split(os.sep)
-
-		#print(comps)
-
-		if 'UnitTest' in comps:
-			#print('skip')
-			continue
-		elif 'cppwinrt' in comps:
-			#print('skip')
-			continue
-
-		newpaths.append(path)
-	return newpaths
-
-
-def dedup_paths(paths: List[str]) -> List[str]:
-	return sorted(set(paths), key=lambda x: paths.index(x))
+	return incpath
 
 
 def run_h2inc(filepath: str, includes: List[str]) -> str:
@@ -53,32 +34,21 @@ def run_h2inc(filepath: str, includes: List[str]) -> str:
 	with tempfile.TemporaryDirectory() as tmpdir:
 		procpath = os.path.join(tmpdir, "_h2inc.tmp")
 
-		env_includes = remove_paths([p for p in os.environ.get('INCLUDE', '').split(';') if p != ''])
-
-		arg_includes = [get_short_path(path) for path in includes]
-		#env_includes = [get_short_path(path) for path in [p for p in os.environ.get('INCLUDE', '').split(';') if p != '']]
-		env_includes = [get_short_path(path) for path in env_includes]
-
-		env_includes = dedup_paths(env_includes)
-		arg_includes = dedup_paths(arg_includes)
+		incpath = build_local_include_dir(tmpdir, includes)
 
 		params = [
 			h2inc, '-nologo', '-G3', '-Zp4', '-w', '-c', '-WIN32',
-			'-DBASED_FLOAT=1', '-D_WIN32', '-D__VISUALC', '-D__H2INC__', '-D_NO_PROTOTYPES',
-			#'-DWIN32_LEAN_AND_MEAN'
+			'-DBASED_FLOAT=1', '-D_WIN32',
+			'-D__VISUALC', '-D__H2INC__', '-D_NO_PROTOTYPES',
+			#'-DWIN32_LEAN_AND_MEAN',
+			f'-I{incpath}',
+			f'-Fa{procpath}', os.path.basename(filepath)
 		]
-
-		params += [f'-I{path}' for path in arg_includes]
-
-		params += [f'-Fa{procpath}', os.path.basename(filepath)]
 
 		newenv = copy.deepcopy(os.environ)
 		newenv.pop('INCLUDE', None)
-		#if len(env_includes) > 0:
-		#	newenv['INCLUDE'] = ";".join(env_includes)
 
-		#print(f'Executing h2inc: {params}')
-		#print('  INCLUDE: ', env_includes)
+		print(f'Executing h2inc: {params}', file=sys.stderr)
 
 		res = subprocess.run(args=params, env=newenv, cwd=os.path.dirname(filepath))
 		if res.returncode != 0:
@@ -103,7 +73,7 @@ ap.add_argument('outfile')
 
 args = ap.parse_args()
 
-#print(args)
+print(args, file=sys.stderr)
 pass1 = run_h2inc(args.infile, args.include_directories)
 pass2 = fix_h2inc(pass1)
 
