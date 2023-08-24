47962cbc59a2c697ef636806b501cf3e90ac4d00
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-08-24 06:00:03 -0500

    idk

--- dosio/dosio.c
+++ dosio/dosio.c
@@ -1,24 +1,15 @@
 
-#include <string.h>
-
 #include "brender.h"
 #include "dosio.h"
 
 static struct {
 
-	/* pixelmaps */
-	br_pixelmap *screen;
-	br_pixelmap *palette;
+    /* pixelmaps */
+    br_pixelmap *screen;
+    br_pixelmap *palette;
 
-	/* initialized flag */
-	br_boolean initialized;
-
-	/* setup string */
-	char *setup_string;
-	char *driver;
-	br_int_32 width;
-	br_int_32 height;
-	br_int_32 bpp;
+    /* initialized flag */
+    br_boolean initialized;
 
 } dosio;
 
@@ -28,12 +19,11 @@ static struct {
 
 br_error BR_PUBLIC_ENTRY DOSBegin(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 void BR_PUBLIC_ENTRY DOSEnd(void)
 {
-
 }
 
 /*
@@ -42,124 +32,73 @@ void BR_PUBLIC_ENTRY DOSEnd(void)
 
 br_pixelmap *BR_PUBLIC_ENTRY DOSGfxBegin(const char *setup_string)
 {
-	/*
-	 * just to be safe
-	 */
+    /*
+     * just to be safe
+     */
 
-	if (dosio.initialized)
-		return NULL;
-	else
-		BrMemSet(&dosio, 0, sizeof(dosio));
+    if(dosio.initialized)
+        return NULL;
+    else
+        BrMemSet(&dosio, 0, sizeof(dosio));
 
-	/*
-	 * tokenize setup string
-	 */
-	if (setup_string)
-	{
-		const char delim[] = ",";
-		char *token;
+    /*
+     * just to be safe
+     */
+    br_error err = BrDevBeginVar(&dosio.screen, "MCGA", BRT_WIDTH_I32, 320, BRT_HEIGHT_I32, 200, BRT_PIXEL_TYPE_U8,
+                                 BR_PMT_RGB_888, BRT_WINDOW_NAME_CSTR, "BRender DOS Application", BRT_HIDPI_B, BR_TRUE,
+                                 BRT_RESIZABLE_B, BR_TRUE, BRT_OPENGL_B, BR_TRUE, BR_NULL_TOKEN);
+    if(err != BRE_OK) {
+        BrLogError("DOSIO", "Failed to begin device.");
+        return NULL;
+    }
 
-		token = strtok(setup_string, delim);
+    /*
+     * allocate a palette
+     */
+    dosio.palette = BrPixelmapAllocate(BR_PMT_RGBX_888, 1, 256, NULL, BR_PMAF_NORMAL);
 
-		while (token != NULL)
-		{
-			char *temp;
+    /*
+     * set initialized
+     */
+    dosio.initialized = BR_TRUE;
 
-			/*
-			 * get driver string, width, height, bpp
-			 */
-			if (BrStrNICmp(token, "VESA", 4) == 0 || BrStrNICmp(token, "MCGA", 4) == 0)
-			{
-				dosio.driver = BrResStrDup(NULL, token);
-			}
-			else if (BrStrNICmp(token, "B:", 2) == 0)
-			{
-				dosio.bpp = BrStrToL(token + 2, token + BrStrLen(token), 10);
-			}
-			else if (BrStrNICmp(token, "W:", 2) == 0)
-			{
-				dosio.width = BrStrToL(token + 2, token + BrStrLen(token), 10);
-			}
-			else if (BrStrNICmp(token, "H:", 2) == 0)
-			{
-				dosio.height = BrStrToL(token + 2, token + BrStrLen(token), 10);
-			}
-
-			token = strtok(NULL, delim);
-		}
-
-		dosio.setup_string = BrResStrDup(NULL, setup_string);
-	}
-	else
-	{
-		dosio.setup_string = BrResStrDup(NULL, "MCGA,W:320,H:200,B:8");
-	}
-
-	/*
-	 * set defaults if the setup string didnt include them
-	 */
-	if (dosio.driver == NULL)
-		dosio.driver = BrResStrDup(NULL, "MCGA");
-	if (!dosio.bpp)
-		dosio.bpp = 32;
-	if (!dosio.width)
-		dosio.width = 320;
-	if (!dosio.height)
-		dosio.height = 200;
-
-	/*
-	 * allocate a palette
-	 */
-	dosio.palette = BrPixelmapAllocate(BR_PMT_RGBX_888, 1, 256, NULL, BR_PMAF_NORMAL);
-
-	/*
-	 * set initialized
-	 */
-	dosio.initialized = BR_TRUE;
-
-	/*
-	 * return screen buffer
-	 */
-	return dosio.screen;
+    /*
+     * return screen buffer
+     */
+    return dosio.screen;
 }
 
 void BR_PUBLIC_ENTRY DOSGfxEnd(void)
 {
-	/*
-	 * sanity check
-	 */
-	if (!dosio.initialized)
-		return;
+    /*
+     * sanity check
+     */
+    if(!dosio.initialized)
+        return;
 
-	/*
-	 * free strings
-	 */
-	BrResFree(dosio.driver);
-	BrResFree(dosio.setup_string);
+    /*
+     * end renderer
+     */
+    BrRendererEnd();
 
-	/*
-	 * end renderer
-	 */
-	BrRendererEnd();
-
-	/*
-	 * free pixelmaps
-	 */
-	BrPixelmapFree(dosio.screen);
-	BrPixelmapFree(dosio.palette);
+    /*
+     * free pixelmaps
+     */
+    BrPixelmapFree(dosio.screen);
+    BrPixelmapFree(dosio.palette);
 }
 
 void BR_PUBLIC_ENTRY DOSGfxPaletteSet(br_pixelmap *pm)
 {
-	if (pm->type != BR_PMT_RGBX_888 && pm->type != BR_PMT_RGBA_8888)
-		return;
+    if(pm->type != BR_PMT_RGBX_888 && pm->type != BR_PMT_RGBA_8888)
+        return;
 
-	BrPixelmapCopy(dosio.palette, pm);
+    BrPixelmapCopy(dosio.palette, pm);
 }
 
 void BR_PUBLIC_ENTRY DOSGfxPaletteSetEntry(int i, br_colour colour)
 {
-	BrPixelmapPixelSet(dosio.palette, 0, i - dosio.palette->origin_y, colour);
+    BrPixelmapPixelSet(dosio.palette, 0, i - dosio.palette->origin_y, colour);
 }
 
 /*
@@ -168,17 +107,16 @@ void BR_PUBLIC_ENTRY DOSGfxPaletteSetEntry(int i, br_colour colour)
 
 br_error BR_PUBLIC_ENTRY DOSMouseBegin(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 void BR_PUBLIC_ENTRY DOSMouseEnd(void)
 {
-
 }
 
 br_error BR_PUBLIC_ENTRY DOSMouseRead(br_int_32 *mouse_x, br_int_32 *mouse_y, br_uint_32 *mouse_buttons)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 /*
@@ -187,17 +125,17 @@ br_error BR_PUBLIC_ENTRY DOSMouseRead(br_int_32 *mouse_x, br_int_32 *mouse_y, br
 
 br_error BR_ASM_CALL DOSClockBegin(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 br_error BR_ASM_CALL DOSClockEnd(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 br_uint_32 BR_ASM_CALL DOSClockRead(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 /*
@@ -206,22 +144,21 @@ br_uint_32 BR_ASM_CALL DOSClockRead(void)
 
 br_error BR_PUBLIC_ENTRY DOSKeyBegin(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 void BR_PUBLIC_ENTRY DOSKeyEnd(void)
 {
-
 }
 
 br_boolean BR_PUBLIC_ENTRY DOSKeyTest(br_uint_8 scancode, br_uint_8 qualifiers, br_uint_8 repeats)
 {
-	return BR_FALSE;
+    return BR_FALSE;
 }
 
 br_error BR_PUBLIC_ENTRY DOSKeyEnableBIOS(br_boolean flag)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 /*
@@ -230,17 +167,16 @@ br_error BR_PUBLIC_ENTRY DOSKeyEnableBIOS(br_boolean flag)
 
 br_error BR_PUBLIC_ENTRY DOSDivTrapBegin(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 void BR_PUBLIC_ENTRY DOSDivTrapEnd(void)
 {
-
 }
 
 br_uint_32 BR_ASM_CALL DOSDivTrapCount(br_boolean reset)
 {
-	return 0;
+    return 0;
 }
 
 /*
@@ -249,15 +185,14 @@ br_uint_32 BR_ASM_CALL DOSDivTrapCount(br_boolean reset)
 
 br_error BR_PUBLIC_ENTRY DOSEventBegin(void)
 {
-	return BRE_OK;
+    return BRE_OK;
 }
 
 void BR_PUBLIC_ENTRY DOSEventEnd(void)
 {
-
 }
 
 br_boolean BR_PUBLIC_ENTRY DOSEventWait(struct dosio_event *event, br_boolean block)
 {
-	return BR_TRUE;
+    return BR_TRUE;
 }
