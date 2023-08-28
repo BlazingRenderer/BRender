8b14510819edc0177a2a30ace92440f2555d9c1a
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-08-28 08:02:16 -0500

    add device.c to v1compat

---
+++ core/v1compat/device.c
@@ -0,0 +1,19 @@
+
+#include "brender.h"
+
+br_pixelmap *BR_PUBLIC_ENTRY BrDevBeginOld(const char *setup_string)
+{
+    return NULL;
+}
+
+void BR_PUBLIC_ENTRY BrDevEndOld(void)
+{
+}
+
+void BR_PUBLIC_ENTRY BrDevPaletteSetOld(br_pixelmap *pm)
+{
+}
+
+void BR_PUBLIC_ENTRY BrDevPaletteSetEntryOld(int i, br_colour colour)
+{
+}
