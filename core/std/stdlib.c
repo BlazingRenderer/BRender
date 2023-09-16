3123bfa48cde9a2ccd1e48af711f4c3b48fc5397
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-09-16 16:32:19 -0500

    core/std: change function defs to match header

--- core/std/stdlib.c
+++ core/std/stdlib.c
@@ -18,32 +18,32 @@
 
 BR_RCS_ID("$Id: stdlib.c 1.1 1997/12/10 16:41:28 jon Exp $")
 
-int BR_RESIDENT_ENTRY BrMemCmp(const void *s1, const void *s2, size_t n)
+int BR_PUBLIC_ENTRY BrMemCmp(const void *s1, const void *s2, size_t n)
 {
     return memcmp(s1, s2, n);
 }
 
-void *BR_RESIDENT_ENTRY BrMemCpy(void *s1, const void *s2, size_t n)
+void *BR_PUBLIC_ENTRY BrMemCpy(void *s1, const void *s2, size_t n)
 {
     return memcpy(s1, s2, n);
 }
 
-void *BR_RESIDENT_ENTRY BrMemSet(void *s, int c, size_t n)
+void *BR_PUBLIC_ENTRY BrMemSet(void *s, int c, size_t n)
 {
     return memset(s, c, n);
 }
 
-char *BR_RESIDENT_ENTRY BrStrCat(char *s1, const char *s2)
+char *BR_PUBLIC_ENTRY BrStrCat(char *s1, const char *s2)
 {
     return strcat(s1, s2);
 }
 
-int BR_RESIDENT_ENTRY BrStrCmp(const char *s1, const char *s2)
+int BR_PUBLIC_ENTRY BrStrCmp(const char *s1, const char *s2)
 {
     return strcmp(s1, s2);
 }
 
-int BR_RESIDENT_ENTRY BrStrICmp(const char *s1, const char *s2)
+int BR_PUBLIC_ENTRY BrStrICmp(const char *s1, const char *s2)
 {
 #if _MSC_VER
     return stricmp(s1, s2);
@@ -52,22 +52,22 @@ int BR_RESIDENT_ENTRY BrStrICmp(const char *s1, const char *s2)
 #endif
 }
 
-char *BR_RESIDENT_ENTRY BrStrCpy(char *s1, const char *s2)
+char *BR_PUBLIC_ENTRY BrStrCpy(char *s1, const char *s2)
 {
     return strcpy(s1, s2);
 }
 
-br_size_t BR_RESIDENT_ENTRY BrStrLen(const char *s)
+br_size_t BR_PUBLIC_ENTRY BrStrLen(const char *s)
 {
     return strlen(s);
 }
 
-int BR_RESIDENT_ENTRY BrStrNCmp(const char *s1, const char *s2, size_t n)
+int BR_PUBLIC_ENTRY BrStrNCmp(const char *s1, const char *s2, size_t n)
 {
     return strncmp(s1, s2, n);
 }
 
-int BR_RESIDENT_ENTRY BrStrNICmp(const char *s1, const char *s2, size_t n)
+int BR_PUBLIC_ENTRY BrStrNICmp(const char *s1, const char *s2, size_t n)
 {
 #ifdef _MSC_VER
     return strnicmp(s1, s2, n);
@@ -76,77 +76,77 @@ int BR_RESIDENT_ENTRY BrStrNICmp(const char *s1, const char *s2, size_t n)
 #endif
 }
 
-char *BR_RESIDENT_ENTRY BrStrNCpy(char *s1, const char *s2, size_t n)
+char *BR_PUBLIC_ENTRY BrStrNCpy(char *s1, const char *s2, size_t n)
 {
     return strncpy(s1, s2, n);
 }
 
-char *BR_RESIDENT_ENTRY BrStrChr(const char *s1, char c)
+char *BR_PUBLIC_ENTRY BrStrChr(const char *s1, char c)
 {
     return strchr(s1, c);
 }
 
-char *BR_RESIDENT_ENTRY BrStrRChr(const char *s1, char c)
+char *BR_PUBLIC_ENTRY BrStrRChr(const char *s1, char c)
 {
     return strrchr(s1, c);
 }
 
-void BR_RESIDENT_ENTRY BrAbort(void)
+void BR_PUBLIC_ENTRY BrAbort(void)
 {
     abort();
 }
 
-char *BR_RESIDENT_ENTRY BrGetEnv(const char *name)
+char *BR_PUBLIC_ENTRY BrGetEnv(const char *name)
 {
     return getenv(name);
 }
 
-float BR_RESIDENT_ENTRY BrStrToF(const char *nptr, char **endptr)
+float BR_PUBLIC_ENTRY BrStrToF(const char *nptr, char **endptr)
 {
     return (float)strtod(nptr, endptr);
 }
 
-double BR_RESIDENT_ENTRY BrStrToD(const char *nptr, char **endptr)
+double BR_PUBLIC_ENTRY BrStrToD(const char *nptr, char **endptr)
 {
     return strtod(nptr, endptr);
 }
 
-long int BR_RESIDENT_ENTRY BrStrToL(const char *nptr, char **endptr, int base)
+long int BR_PUBLIC_ENTRY BrStrToL(const char *nptr, char **endptr, int base)
 {
     return strtol(nptr, endptr, base);
 }
 
-unsigned long BR_RESIDENT_ENTRY BrStrToUL(const char *nptr, char **endptr, int base)
+unsigned long BR_PUBLIC_ENTRY BrStrToUL(const char *nptr, char **endptr, int base)
 {
     return strtoul(nptr, endptr, base);
 }
 
-br_boolean BR_RESIDENT_ENTRY BrIsAlpha(int c)
+br_boolean BR_PUBLIC_ENTRY BrIsAlpha(int c)
 {
     return isalpha(c);
 }
 
-br_boolean BR_RESIDENT_ENTRY BrIsDigit(int c)
+br_boolean BR_PUBLIC_ENTRY BrIsDigit(int c)
 {
     return isdigit(c);
 }
 
-br_boolean BR_RESIDENT_ENTRY BrIsSpace(int c)
+br_boolean BR_PUBLIC_ENTRY BrIsSpace(int c)
 {
     return isspace(c);
 }
 
-br_boolean BR_RESIDENT_ENTRY BrIsPrint(int c)
+br_boolean BR_PUBLIC_ENTRY BrIsPrint(int c)
 {
     return isprint(c);
 }
 
-br_int_32 BR_RESIDENT_ENTRY BrVSprintf(char *buf, const char *fmt, va_list args)
+br_int_32 BR_PUBLIC_ENTRY BrVSprintf(char *buf, const char *fmt, va_list args)
 {
     return vsprintf(buf, fmt, args);
 }
 
-br_int_32 BR_RESIDENT_ENTRY BrVSprintfN(char *buf, br_size_t buf_size, const char *fmt, va_list args)
+br_int_32 BR_PUBLIC_ENTRY BrVSprintfN(char *buf, br_size_t buf_size, const char *fmt, va_list args)
 {
     unsigned int n;
     char         tmp[512];
@@ -163,7 +163,7 @@ br_int_32 BR_RESIDENT_ENTRY BrVSprintfN(char *buf, br_size_t buf_size, const cha
     return n;
 }
 
-br_int_32 BR_RESIDENT_ENTRY BrVSScanf(const char *buf, const char *fmt, va_list args)
+br_int_32 BR_PUBLIC_ENTRY BrVSScanf(const char *buf, const char *fmt, va_list args)
 {
     return vsscanf(buf, fmt, args);
 }
