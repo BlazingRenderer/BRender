a5588fda4bb19a5ebbdb7546e96dbcda12d658f2
Zane van Iperen <zane@zanevaniperen.com>
2024-04-30 23:44:51 +1000

    examples/samples/minzs: port to br_demo framework

--- examples/samples/minzs/minzs.c
+++ examples/samples/minzs/minzs.c
@@ -1,84 +1,76 @@
-/*
- * Copyright (c) 1995 Argonaut Technologies Limited. All rights reserved.
- *
- * $Id:  $
- * $Locker:  $
- *
- */
 #include <stddef.h>
-#include <stdio.h>
+#include <brdemo.h>
 
-#include "brender.h"
-#include "dosio.h"
-
-/*
- * The screen and the offscreen buffer
- */
-br_pixelmap *screen_buffer, *back_buffer;
-
-/*
- * Pointer to primitive heap
- */
-void *primitives;
-
-/*
- * The actors in the world
- */
-br_actor *observer, *world;
-int       main(int argc, char **argv)
+static br_error MinZSInit(br_demo *demo)
 {
-    br_actor    *a;
-    br_pixelmap *palette;
-    int          i;
+    br_actor  *a;
+    br_camera *camera_data;
 
     /*
-     * Setup renderer and screen
+     * The demo framework creates a depth buffer, but we don't want it.
      */
-    BrBegin();
-    primitives = BrMemAllocate(10000, BR_MEMORY_APPLICATION);
-    InitializeSampleZSort(&screen_buffer, &back_buffer, primitives, 10000);
+    BrPixelmapFree(demo->depth_buffer);
+    demo->depth_buffer = NULL;
 
-    /*
-     * Build the world
-     */
-    world    = BrActorAllocate(BR_ACTOR_NONE, NULL);
-    observer = CreateSampleCamera(world);
-    BrLightEnable(BrActorAdd(world, BrActorAllocate(BR_ACTOR_LIGHT, NULL)));
+    demo->camera = BrActorAdd(demo->world, BrActorAllocate(BR_ACTOR_CAMERA, NULL));
 
-    a                    = BrActorAdd(world, BrActorAllocate(BR_ACTOR_MODEL, NULL));
+    camera_data              = demo->camera->type_data;
+    demo->order_table->min_z = camera_data->hither_z;
+    demo->order_table->max_z = camera_data->yon_z;
+
+    BrLightEnable(BrActorAdd(demo->world, BrActorAllocate(BR_ACTOR_LIGHT, NULL)));
+
+    a                    = BrActorAdd(demo->world, BrActorAllocate(BR_ACTOR_MODEL, NULL));
     a->t.type            = BR_TRANSFORM_EULER;
     a->t.t.euler.e.order = BR_EULER_ZXY_S;
     BrVector3Set(&a->t.t.euler.t, BR_SCALAR(0.0), BR_SCALAR(0.0), BR_SCALAR(-6.0));
 
+    demo->user = a;
+    return BRE_OK;
+}
+
+static void MinZSUpdate(br_demo *demo, br_scalar dt)
+{
+    br_actor *a = demo->user;
+
     /*
      * Tumble the actor around
      */
-    float dt;
-    i = 0;
-    while(UpdateSample(observer, &dt)) {
-        /*
-         * Clear the buffers
-         */
-        BrPixelmapFill(back_buffer, 0);
+    float aa = BrScalarToFloat(BrAngleToScalar(a->t.t.euler.e.a));
+    float bb = BrScalarToFloat(BrAngleToScalar(a->t.t.euler.e.a));
+    float cc = BrScalarToFloat(BrAngleToScalar(a->t.t.euler.e.a));
 
-        BrZsSceneRender(world, observer, back_buffer);
-        BrPixelmapDoubleBuffer(screen_buffer, back_buffer);
+    aa = fmodf(aa + (dt * 1) / 25.0f, 1.0f);
+    bb = fmodf(bb + (dt * 2) / 25.0f, 1.0f);
+    cc = fmodf(cc + (dt * 3) / 25.0f, 1.0f);
 
-        a->t.t.euler.e.a = BR_ANGLE_DEG(i);
-        a->t.t.euler.e.b = BR_ANGLE_DEG(i * 2);
-        a->t.t.euler.e.c = BR_ANGLE_DEG(i * 3);
+    a->t.t.euler.e.a = BrScalarToAngle(aa);
+    a->t.t.euler.e.b = BrScalarToAngle(bb);
+    a->t.t.euler.e.c = BrScalarToAngle(cc);
+}
 
-        i++;
-        if(i >= 360)
-            i = 0;
-    }
+static void MinZSRender(br_demo *demo)
+{
+    BrPixelmapFill(demo->colour_buffer, 0);
 
     /*
-     * Close down
+     * Do a Z-Sort render (no depth buffer).
      */
-    BrZsEnd();
-    DOSGfxEnd();
-    BrEnd();
+    BrRendererFrameBegin();
+    BrZsSceneRender(demo->world, demo->camera, demo->colour_buffer);
+    BrRendererFrameEnd();
+}
 
-    return 0;
+const static br_demo_dispatch dispatch = {
+    .init          = MinZSInit,
+    .process_event = BrDemoDefaultProcessEvent,
+    .update        = MinZSUpdate,
+    .render        = MinZSRender,
+    .on_resize     = BrDemoDefaultOnResize,
+    .destroy       = BrDemoDefaultDestroy,
+};
+
+int main(int argc, char **argv)
+{
+    return BrDemoRun("BRender Minimal Z-Sort Demo", 1280, 720, &dispatch);
 }
