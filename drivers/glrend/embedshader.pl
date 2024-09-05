92009a3b84be6e9c04665c0c5c3a1e9f5fa22242
Zane van Iperen <zane@zanevaniperen.com>
2024-09-06 00:29:32 +0900

    drivers/glrend/embedshader: rudimentary #include support
    This is how compilers are born.

--- drivers/glrend/embedshader.pl
+++ drivers/glrend/embedshader.pl
@@ -5,10 +5,26 @@ open(OUTPUT, '>', $ARGV[2]);
 
 printf OUTPUT "static const GLchar %s[] = {\n", $ARGV[0];
 
-open(INPUT, '<', $ARGV[1]);
-while (<INPUT>) {
-    print OUTPUT ("    ", join(", ", map ord, split(//, $_)), ", \n");
+sub write_hex {
+    print OUTPUT ("    ", join(", ", map ord, split(//, $_[0])), ", \n");
 }
-close(INPUT);
+
+sub read_file {
+    my $fh;
+    open($fh, '<', $_[0]) || die "Can't open file '$_[0]'";
+    while (my $line = <$fh>) {
+        my $ifile;
+        if($line =~ /^\s*#include\s*"(.+)(?{ $ifile = $^N })"\s*$/) {
+            write_hex("// BEGIN INCLUDE $ifile\n");
+            read_file($ifile);
+            write_hex("// END INCLUDE $ifile\n");
+        } else {
+            write_hex($line);
+        }
+    }
+    close($fh);
+}
+
+read_file($ARGV[1]);
 
 printf OUTPUT "    0\n};\n";
