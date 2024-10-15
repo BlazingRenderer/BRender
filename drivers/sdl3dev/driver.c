dcd3e39e7faaf85ebe64b6b272612cfe3cdc3240
Zane van Iperen <zane@zanevaniperen.com>
2024-10-16 02:47:13 +0900

    drivers/sdl3dev: port to sdl3

--- drivers/sdl3dev/driver.c
+++ drivers/sdl3dev/driver.c
@@ -5,7 +5,7 @@ br_token brt_sdl_surface_h = BRT_NONE;
 /*
  * Main entry point for device - this may get redefined by the build system.
  */
-br_device *BR_EXPORT BrDrv1SDL2Begin(const char *arguments)
+br_device *BR_EXPORT BrDrv1SDL3Begin(const char *arguments)
 {
     br_device *device;
     (void)arguments;
@@ -13,12 +13,12 @@ br_device *BR_EXPORT BrDrv1SDL2Begin(const char *arguments)
     /*
      * Set up device
      */
-    if((device = DeviceSDL2Allocate("SDL2")) == NULL)
+    if((device = DeviceSDL3Allocate("SDL3")) == NULL)
         return NULL;
 
     brt_sdl_surface_h = BrTokenCreate("SDL_SURFACE_H", BR_NULL_TOKEN);
 
-    if(OutputFacilitySDL2CreateGeneric(device) == NULL) {
+    if(OutputFacilitySDL3CreateGeneric(device) == NULL) {
         /*
          * If nothing is available, then don't admit to being a device
          */
@@ -32,6 +32,6 @@ br_device *BR_EXPORT BrDrv1SDL2Begin(const char *arguments)
 #ifdef DEFINE_BR_ENTRY_POINT
 br_device *BR_EXPORT BrDrv1Begin(const char *arguments)
 {
-    return BrDrv1SDL2Begin(arguments);
+    return BrDrv1SDL3Begin(arguments);
 }
 #endif
