5110e472e4b80de86ccdbcb6ab4faa150e50eecb
Zane van Iperen <zane@zanevaniperen.com>
2025-08-19 19:13:34 +1000

    core/fw/brhash: don't build XXH3 if we're not using it
    Fixes alignment errors with djgpp.

--- core/fw/brhash.c
+++ core/fw/brhash.c
@@ -5,7 +5,18 @@
  *
  * Basic Hash Functions.
  */
+#include <limits.h>
 #include "fw.h"
+
+#define USE_XXH64 (defined(_WIN64) || (LONG_MAX == 9223372036854775807L))
+
+/**
+ * If not using the 64-bit variant of the hash, don't even compile it.
+ * Fixes issues on djgpp.
+ */
+#if !USE_XXH64
+#define XXH_NO_LONG_LONG
+#endif
 
 #define XXH_STATIC_LINKING_ONLY
 #define XXH_IMPLEMENTATION
@@ -20,7 +31,7 @@ br_hash BR_RESIDENT_ENTRY BrHash(const void *data, size_t size)
      * Use whichever one matches the machine's native word type.
      * See https://stackoverflow.com/a/67223865
      */
-#if defined(_WIN64) || LONG_MAX == 9223372036854775807L
+#if USE_XXH64
     return XXH3_64bits_withSeed(data, size, 0);
 #else /* LONG_MAX == 2147483647L */
     return XXH32(data, size, 0);
