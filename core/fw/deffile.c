b75d8ab961436363af1d514febc8123072954f28
Zane van Iperen <zane@zanevaniperen.com>
2025-08-22 02:04:08 +1000

    core/fw/deffile: correct stubs
    Fixes compiler warnings.

--- core/fw/deffile.c
+++ core/fw/deffile.c
@@ -13,33 +13,92 @@ static br_uint_32 BrNullAttributes(void)
     return 0;
 }
 
-static void BrNullOther(void)
+static void *BR_CALLBACK BrNullOpenRead(const char *name, br_size_t n_magics, br_mode_test_cbfn *identify, int *mode_result)
 {
-    BR_FATAL0("Invald file system call");
+    BR_FATAL0("Invalid file system call");
+    return NULL;
+}
+
+static void *BR_CALLBACK BrNullOpenWrite(const char *name, int mode)
+{
+    BR_FATAL0("Invalid file system call");
+    return NULL;
+}
+
+static void BR_CALLBACK BrNullClose(void *f)
+{
+    BR_FATAL0("Invalid file system call");
+}
+
+static int BR_CALLBACK BrNullEof(void *f)
+{
+    BR_FATAL0("Invalid file system call");
+    return -1;
+}
+
+static int BR_CALLBACK BrNullGetChar(void *f)
+{
+    BR_FATAL0("Invalid file system call");
+    return -1;
+}
+
+static void BR_CALLBACK BrNullPutChar(int c, void *f)
+{
+    BR_FATAL0("Invalid file system call");
+}
+
+static br_size_t BR_CALLBACK BrNullRead(void *buf, br_size_t size, br_size_t n, void *f)
+{
+    BR_FATAL0("Invalid file system call");
+    return 0;
+}
+
+static br_size_t BR_CALLBACK BrNullWrite(const void *buf, br_size_t size, br_size_t n, void *f)
+{
+    BR_FATAL0("Invalid file system call");
+    return 0;
+}
+
+static br_size_t BR_CALLBACK BrNullGetLine(char *buf, br_size_t buf_len, void *f)
+{
+    BR_FATAL0("Invalid file system call");
+    return 0;
+}
+
+static void BR_CALLBACK BrNullPutLine(const char *buf, void *f)
+{
+    BR_FATAL0("Invalid file system call");
+}
+
+static void BR_CALLBACK BrNullAdvance(br_size_t count, void *f)
+{
+    BR_FATAL0("Invalid file system call");
+}
+
+static void *BR_CALLBACK BrNullLoad(void *res, const char *name, br_size_t *size)
+{
+    BR_FATAL0("Invalid file system call");
+    return NULL;
 }
 
 /*
  * Filesystem structure
  */
 br_filesystem BrNullFilesystem = {
-    "Null Filesystem", /* identifier */
-
-    BrNullAttributes,    /* attributes */
-    (void *)BrNullOther, /* open_read  */
-    (void *)BrNullOther, /* openwrite  */
-    (void *)BrNullOther, /* close      */
-    (void *)BrNullOther, /* eof		  */
-
-    (void *)BrNullOther, /* getchar	  */
-    (void *)BrNullOther, /* putchar	  */
-
-    (void *)BrNullOther, /* read		  */
-    (void *)BrNullOther, /* write	  */
-
-    (void *)BrNullOther, /* getline	  */
-    (void *)BrNullOther, /* putline	  */
-
-    (void *)BrNullOther, /* advance	  */
+    .identifier = "Null Filesystem",
+    .attributes = BrNullAttributes,
+    .open_read  = BrNullOpenRead,
+    .open_write = BrNullOpenWrite,
+    .close      = BrNullClose,
+    .eof        = BrNullEof,
+    .getchr     = BrNullGetChar,
+    .putchr     = BrNullPutChar,
+    .read       = BrNullRead,
+    .write      = BrNullWrite,
+    .getline    = BrNullGetLine,
+    .putline    = BrNullPutLine,
+    .advance    = BrNullAdvance,
+    .load       = BrNullLoad,
 };
 
 // Nope, we're using std now
