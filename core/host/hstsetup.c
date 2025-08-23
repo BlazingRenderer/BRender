8f7c6d245f667fe9af7fb68a3bb66ec4db99f70b
Zane van Iperen <zane@zanevaniperen.com>
2025-08-23 17:20:00 +1000

    core/host/hstsetup: restore 1.3.2's identifier for DJGPP

--- core/host/hstsetup.c
+++ core/host/hstsetup.c
@@ -39,6 +39,8 @@ static host_info hostInfo = {
     .size = sizeof(host_info),
 #if defined(__WIN_32__)
     .identifier = "Microsoft WIN32",
+#elif defined(__GO32__)
+    .identifier = "DJGPP GO32",
 #elif defined(__DOS__)
     .identifier = "Generic DOS",
 #else
