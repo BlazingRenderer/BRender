5fab6b975987cc986ab7e5f8f31cb7f7f55c1016
Zane van Iperen <zane@zanevaniperen.com>
2026-03-17 00:50:48 +1000

    nix/uasm: fix after nixpkgs update

--- nix/uasm/default.nix
+++ nix/uasm/default.nix
@@ -1,13 +1,10 @@
-{ pkgs }:
-(pkgs.uasm.override { gcc13Stdenv = pkgs.stdenv; }).overrideAttrs(old: {
-  makeFlags = old.makeFlags ++ [ "DJGPP=1" ];
+{ uasm }:
+uasm.overrideAttrs(old: {
+  makeFlags = (old.makeFlags or []) ++ [ "DJGPP=1" ];
   patches = (old.patches or []) ++ [
     ./0001-Makefile-Linux-GCC-OSX-Clang-64-allow-enabling-djgpp.patch
     ./0002-H-globals-add-SFORMAT_DJGPP-from-JWasm.patch
     ./0003-fixup-fix-djgpp-compile-error.patch
   ];
   meta = old.meta // { license = []; };
-
-  # Get the damned thing compiling with GCC 15
-  env.CFLAGS = "-std=c99 -Wno-incompatible-pointer-types -Wno-implicit-function-declaration -Wno-int-conversion";
 })
