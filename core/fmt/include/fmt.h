512830ac23af34a41330ec3a00c11048800d74d4
Dillon Jones <dillon.z.jones@gmail.com>
2026-04-03 23:20:35 -0400

    core/fmt: guard animation structs from H2INC
    The glTF animation structs cause MASM error A2037 when H2INC converts
    them for drv.inc via the drv.h -> brddi.h -> brender.h -> brv1db.h ->
    fmt.h include chain. Wrap in #if !defined(__H2INC__) since these
    structs are only needed by C code.

--- core/fmt/include/fmt.h
+++ core/fmt/include/fmt.h
@@ -164,8 +164,9 @@ typedef struct br_vue {
 } br_vue;
 
 /*
- * glTF animation structures
+ * glTF animation structures (not visible to H2INC/MASM)
  */
+#if !defined(__H2INC__)
 typedef struct br_gltf_keyframes {
     br_int_32 count;
     float    *times;
@@ -195,6 +196,7 @@ typedef struct br_gltf_anim_data {
     br_int_32           nanims;
     br_int_32           active_anim;  /* index of animation to play, or -1 for all */
 } br_gltf_anim_data;
+#endif /* !__H2INC__ */
 
 /*
  * Image type enumerations
