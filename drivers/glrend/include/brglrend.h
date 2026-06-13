48e877b22d8890887381e55c6042b94043e18cbf
Zane van Iperen <zane@zanevaniperen.com>
2026-06-13 19:25:19 +1000

    drivers/glrend: move BRT_OPENGL_EXTENSION{S_PL,_COUNT_I32} to proper tokens

--- drivers/glrend/include/brglrend.h
+++ drivers/glrend/include/brglrend.h
@@ -11,8 +11,6 @@ extern "C" {
 
 extern br_token BRT_OPENGL_OPAQUE_DRAW_COUNT_U32;
 extern br_token BRT_OPENGL_TRANSPARENT_DRAW_COUNT_U32;
-extern br_token BRT_OPENGL_NUM_EXTENSIONS_I32;
-extern br_token BRT_OPENGL_EXTENSIONS_PL;
 
 #ifdef __cplusplus
 }
