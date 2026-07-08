7abdf46295dbb5f3d3d5a70cd71777c8df47a5e1
Zane van Iperen <zane@zanevaniperen.com>
2026-07-09 00:24:39 +1000

    drivers/glrend1x/template: remove unused template storage

--- drivers/glrend1x/template.h
+++ drivers/glrend1x/template.h
@@ -12,43 +12,25 @@ struct device_templates {
     /*
      * Object query templates
      */
-    struct br_tv_template *deviceTemplate;
-    struct br_tv_template *outputFacilityTemplate;
-    struct br_tv_template *devicePixelmapTemplate;
-    struct br_tv_template *devicePixelmapSubTemplate;
-    struct br_tv_template *devicePixelmapFrontTemplate;
-    struct br_tv_template *deviceClutTemplate;
-    struct br_tv_template *rendererFacilityTemplate;
-    struct br_tv_template *rendererTemplate;
-    struct br_tv_template *rendererStateStoredTemplate;
-    struct br_tv_template *bufferStoredTemplate;
-    struct br_tv_template *geometryV1ModelTemplate;
-    struct br_tv_template *geometryV1BucketsTemplate;
-    struct br_tv_template *geometryPrimitivesTemplate;
-    struct br_tv_template *geometryLightingTemplate;
-    struct br_tv_template *geometryStoredTemplate;
-    struct br_tv_template *primitiveStateTemplate;
+    br_tv_template *deviceTemplate;
+    br_tv_template *outputFacilityTemplate;
+    br_tv_template *devicePixelmapTemplate;
+    br_tv_template *devicePixelmapSubTemplate;
+    br_tv_template *devicePixelmapFrontTemplate;
+    br_tv_template *deviceClutTemplate;
+    br_tv_template *primitiveStateTemplate;
 
     /*
      * Argument processing templates
      */
-    struct br_tv_template *pixelmapNewTemplate;
-    struct br_tv_template *pixelmapMatchTemplate;
-    struct br_tv_template *rendererNewTemplate;
+    br_tv_template *pixelmapNewTemplate;
+    br_tv_template *pixelmapMatchTemplate;
 
     /*
      * Parts templates
      */
-    struct br_tv_template *partCullTemplate;
-    struct br_tv_template *partSurfaceTemplate;
-    struct br_tv_template *partMatrixTemplate;
-    struct br_tv_template *partEnableTemplate;
-    struct br_tv_template *partHiddenSurfaceTemplate;
-    struct br_tv_template *partBoundsTemplate;
-    struct br_tv_template *partLightTemplate;
-    struct br_tv_template *partClipTemplate;
-    struct br_tv_template *partPrimitiveTemplate;
-    struct br_tv_template *partOutputTemplate;
+    br_tv_template *partPrimitiveTemplate;
+    br_tv_template *partOutputTemplate;
 };
 
 #ifdef __cplusplus
